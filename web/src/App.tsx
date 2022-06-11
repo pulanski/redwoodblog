36df93d8ac1e761e92311e0df90499b33444fca6
Josh Kersey <joshuakersey50@gmail.com>
2022-06-11 16:28:54 -0400

    feat(posts): add posts scaffold
    add posts scaffold for crud operations

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,56 +5,26 @@ import {
   ColorSchemeProvider,
   MantineProvider,
 } from '@mantine/core'
-import { SpotlightAction, SpotlightProvider } from '@mantine/spotlight'
+import { SpotlightProvider } from '@mantine/spotlight'
 import {
-  Home,
   Search,
-  Wallpaper,
-  Notebook,
-  Bookmarks,
 } from 'tabler-icons-react'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import './scaffold.css'
 import './index.css'
-import { navigate, routes } from '@redwoodjs/router'
 import { useState } from 'react'
-
-const actions: SpotlightAction[] = [
-  {
-    title: 'Home',
-    description: 'Go to home page',
-    onTrigger: () => navigate(routes.home()),
-    icon: <Home size={18} />,
-  },
-  {
-    title: 'New Post',
-    description: 'Let the world know what you are thinking',
-    onTrigger: () => navigate('/posts'),
-    icon: <Wallpaper size={18} />,
-  },
-  {
-    title: 'Collection',
-    description: 'View all of your created blog posts',
-    onTrigger: () => navigate('/collection'),
-    icon: <Notebook size={18} />,
-  },
-  {
-    title: 'Bookmarks',
-    description: 'View all of your saved blog posts',
-    onTrigger: () => navigate('/bookmarks'),
-    icon: <Bookmarks size={18} />,
-  },
-  {
-    title: 'Settings',
-    description: 'Tweak your settings to your liking',
-    onTrigger: () => navigate(routes.settings()),
-    icon: <Notebook size={18} />,
-  },
-]
+import { useHotkeys } from '@mantine/hooks'
+import { actions } from './SpotlightActions'
 
 export function App() {
+
+  useHotkeys([
+    ['mod+J', () => toggleColorScheme()],
+  ])
+
   const [colorScheme, setColorScheme] = useState<ColorScheme>('dark')
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
---
+++ web/src/SpotlightActions.tsx
@@ -0,0 +1,40 @@
+import { SpotlightAction } from '@mantine/spotlight';
+import {
+  Home, Wallpaper,
+  Notebook,
+  Bookmarks
+} from 'tabler-icons-react';
+import { navigate, routes } from '@redwoodjs/router';
+
+export const actions: SpotlightAction[] = [
+  {
+    title: 'Home',
+    description: 'Go to home page',
+    onTrigger: () => navigate(routes.home()),
+    icon: <Home size={18} />,
+  },
+  {
+    title: 'New Post',
+    description: 'Let the world know what you are thinking',
+    onTrigger: () => navigate('/posts'),
+    icon: <Wallpaper size={18} />,
+  },
+  {
+    title: 'Collection',
+    description: 'View all of your created blog posts',
+    onTrigger: () => navigate('/collection'),
+    icon: <Notebook size={18} />,
+  },
+  {
+    title: 'Bookmarks',
+    description: 'View all of your saved blog posts',
+    onTrigger: () => navigate('/bookmarks'),
+    icon: <Bookmarks size={18} />,
+  },
+  {
+    title: 'Settings',
+    description: 'Tweak your settings to your liking',
+    onTrigger: () => navigate(routes.settings()),
+    icon: <Notebook size={18} />,
+  },
+];
